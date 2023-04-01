c11867c6eb09642d7b85d99194097dc19b11106a
Nitesh-Saini1 <saini.nitesh1989@gmail.com>
2023-04-01 13:26:29 +0530

    ABC

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Annoucments from "../components/Annoucments";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
+      <Annoucments />
       <Navbar />
+      <Slider />
     </div>
   );
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined";
+import ArrowRightOutlinedIcon from "@mui/icons-material/ArrowRightOutlined";
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: coral;
+  position: relative;
+`;
 
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+
+  margin: auto;
+`;
+const left = {
+  left: "10px",
+};
 const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <Container>
+      <Arrow className={left}>
+        <ArrowLeftOutlinedIcon></ArrowLeftOutlinedIcon>
+      </Arrow>
+      <Arrow className="right">
+        <ArrowRightOutlinedIcon></ArrowRightOutlinedIcon>
+      </Arrow>
+    </Container>
+  );
 };
 
 export default Slider;
